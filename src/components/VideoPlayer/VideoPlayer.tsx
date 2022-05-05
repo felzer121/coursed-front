ea6873efd330d87698b3ccc6d09a154f5eaebd9d
F <>
2022-05-05 22:31:53 +0300

    filling progress bar

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -9,16 +9,16 @@ import FullscreenIcon from '@mui/icons-material/Fullscreen';
 
 const settings = [
   {
-    durationSeconds: 50,
-    name: 'Объявление темы'  
+    start: 0,
+    description: 'Объявление темы'  
   },
   {
-    durationSeconds: 250,
-    name: 'Объявление темы'  
+    start: 50,
+    description: 'Объявление темы'  
   },
   {
-    durationSeconds: 300,
-    name: 'Метод для компонента'   
+    start: 250,
+    description: 'Метод для компонента'   
   }
 ]
 
@@ -27,8 +27,8 @@ interface VideoPlayerProps {
   heigth?: number
 }
 interface videoArgType {
-  durationSeconds: number
-  name: string
+  start: number
+  description: string
   width: number
 }
 interface videoStatus {
@@ -37,7 +37,6 @@ interface videoStatus {
 
 
 function toggleFullScreen(video) {
-  console.log(video.current.fullscreenElement)
   if (!document.fullscreenElement &&    // alternative standard method
           !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
           if (document.documentElement.requestFullscreen) {
@@ -64,23 +63,57 @@ function toggleFullScreen(video) {
 }
 
 
+function formatTime(timeInSeconds) {
+  const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+  return {
+    minutes: result.substr(3, 2),
+    seconds: result.substr(6, 2),
+  };
+};
+
 const VideoPlayer = ({width, heigth}: VideoPlayerProps) => {
 
   const video = React.useRef<HTMLVideoElement | null>(null)
   const [duration, setDuration] = React.useState<number | null>(null)
   const [videoArg, setVideoArg] = React.useState<videoArgType[] | null>(null)
   const [videoStatus, setVideoStatus] = React.useState<videoStatus>({onPause: true})
+  const [currentTime, setCurrentTime] = React.useState<number>(0)
 
   !!width ? width : width = 400
 
   React.useEffect(() => {
     if(!!video.current && !!duration) {
       video.current.controls = false;
-      setVideoArg(settings.map(item => {
-          return {...item, width: item.durationSeconds / (duration / 100)}
+      setVideoArg(settings.map((item, index) => { 
+        let end = 0
+        const start = item.start
+        if(settings.length-1 === index)
+          end = duration
+        else 
+          end = settings[index + 1].start
+        return {...item, width: (end - start) / (duration / 100)}
       }))
     }
   }, [duration])
+  
+  const viewsElem = (index: number): any => {
+    let end = 0
+    const start = settings[index].start
+      if(settings.length-1 === index)
+        end = duration
+      else 
+        end = settings[index + 1].start
+      let width = (currentTime - start) / ((end - start) / 100)
+      width = width > 100 ? 100 : width < 0 ? 0 : width
+      return {width: `${width}%`}
+  }
+
+
+  if(!!video.current)
+    video.current.ontimeupdate = (event) => {
+      setCurrentTime(event.target.currentTime)
+    };
   
   const handleLoadedMetadata = () => {
     const videoEl = video.current;
@@ -89,20 +122,26 @@ const VideoPlayer = ({width, heigth}: VideoPlayerProps) => {
     setVideoStatus({onPause: videoEl?.paused})
   };
 
+  const getTime = (duration: number) => {
+    const videoDuration = Math.round(duration);
+    const time = formatTime(videoDuration);
+    return `${time.minutes}:${time.seconds}`;
+  }
+
   const handleController = (type: string) => {
-    switch(type){
+    switch(type) {
       case 'playPause' :
         !!video.current?.paused ? video.current?.play() : video.current?.pause()
         setVideoStatus({onPause: !!video.current?.paused})
         break;
       case 'fullscreen' :
         video?.current?.requestFullscreen()
-    }
+    } 
   }
 
   const handleProgress = (e) => {
     if(!!video.current) {
-      var pos = (e.pageX  - (video.current.offsetLeft + video.current.offsetParent?.offsetLeft)) / video.current.offsetWidth;
+      const pos = (e.pageX  - (video.current.offsetLeft + video.current.offsetParent?.offsetLeft)) / video.current.offsetWidth;
 		  video.current.currentTime = pos * video.current.duration;
     }
   }
@@ -118,17 +157,23 @@ const VideoPlayer = ({width, heigth}: VideoPlayerProps) => {
           <div className='videoBackground' />
           <div id="video-controls" className="controls" data-state="visible">
             <div className='progress' onClick={handleProgress}>
-              {videoArg.map(item => (
-                <div className='progress-bar-item' style={{width: `${item.width}%`}} />
+              {videoArg.map((item, index) => (
+                <div className='progress-bar-item' style={{width: `${item.width}%`}} >
+                  <div style={{...viewsElem(index), background: 'red', height: '100%'}} />
+                </div>
               ))}
             </div>
-            <div>
-              <IconButton data-state="play" onClick={()=>handleController('playPause')}>
-                {videoStatus?.onPause ? <PlayArrowIcon style={{color: '#fff'}} /> :
-                <PauseIcon style={{color: '#fff'}} />}
-              </IconButton>
+            <div className='video-controllers'>
+              <div>
+                <IconButton data-state="play" onClick={()=>handleController('playPause')}>
+                  {videoStatus?.onPause ? <PlayArrowIcon style={{color: '#fff'}} /> :
+                  <PauseIcon style={{color: '#fff'}} />}
+                </IconButton>
+                <span>{getTime(currentTime)} / </span>
+                <span>{getTime(duration)}</span>
+              </div>
               <IconButton data-state="fullscreen" onClick={()=>handleController('fullscreen')}>
-                <FullscreenIcon />
+                <FullscreenIcon style={{color: '#fff'}} />
               </IconButton>
             </div>
             
